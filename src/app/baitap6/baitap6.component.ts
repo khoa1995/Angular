ec633c42f0ac0bcb8fe296f590d53c312e0bf305
FL Khoa Phan <phannguyendangkhoa1995@gmail.com>
2020-10-27 21:38:03 +0700

    commit code

---
+++ src/app/baitap6/baitap6.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-baitap6',
+  templateUrl: './baitap6.component.html',
+  styleUrls: ['./baitap6.component.scss']
+})
+export class Baitap6Component implements OnInit {
+  dssp: any[] = [
+    {maSP:'DT' , tenSP: 'Dien thoai', gia: 10000},
+    {maSP:'TV' , tenSP: 'Tivi', gia: 10000},
+    {maSP:'LV' , tenSP: 'Lo vi song', gia: 10000},
+  ]
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  themSP() {
+    
+  }
+}
